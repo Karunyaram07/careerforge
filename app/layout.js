b99e375d63db0e1da90cd8a0eff6efae604c1273
KarunyaRam <sprkarunya986@gmail.com>
2026-06-12 20:20:29 +0530

    chore: initialize repository

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider"
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +23,28 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+          {/*header*/}
+          <Header/>
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <footer className="bg-muted/50 py-14">
+            <p className="flex flex-col justify-center items-center">Made with ❤️ by Next.Js</p>
+          </footer>
+          {/*footer*/}
+        
+        </ThemeProvider>
+        
+        </body>
     </html>
   );
 }
